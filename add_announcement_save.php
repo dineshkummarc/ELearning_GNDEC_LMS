c3f3c697494eb6b9d71e0ca33b6e393c49e5c292
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 16:00:17 +0530

    update

--- add_announcement_save.php
+++ add_announcement_save.php
@@ -6,8 +6,8 @@ include('session.php');
 		$N = count($id);
 		for($i=0; $i < $N; $i++)
 		{			
-			mysql_query("insert into teacher_class_announcements (teacher_class_id,teacher_id,content,date) values('$id[$i]','$session_id','$content',NOW())")or die(mysql_error());
-			mysql_query("insert into notification (teacher_class_id,notification,date_of_notification,link) value('$id[$i]','Add Annoucements',NOW(),'announcements_student.php')")or die(mysql_error());
+			mysqli_query($GLOBALS["___mysqli_ston"], "insert into teacher_class_announcements (teacher_class_id,teacher_id,content,date) values('$id[$i]','$session_id','$content',NOW())")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+			mysqli_query($GLOBALS["___mysqli_ston"], "insert into notification (teacher_class_id,notification,date_of_notification,link) value('$id[$i]','Add Annoucements',NOW(),'announcements_student.php')")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 		}
 ?>
 
