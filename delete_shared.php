7d94a473a2576ee77c3f35a73c485cc90d60dd95
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 15:19:51 +0530

    update

--- delete_shared.php
+++ delete_shared.php
@@ -8,8 +8,8 @@ $class_id = $_POST['teacher_class_id'];
 $N = count($id);
 for($i=0; $i < $N; $i++)
 {
-	$result = mysql_query("select * from teacher_shared  where teacher_shared_id = '$id[$i]' ")or die(mysql_error());
-	while($row = mysql_fetch_array($result)){
+	$result = mysqli_query($GLOBALS["___mysqli_ston"], "select * from teacher_shared  where teacher_shared_id = '$id[$i]' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+	while($row = mysqli_fetch_array($result)){
 	
 	$fname = $row['fname'];
 	$floc = $row['floc'];
@@ -18,7 +18,7 @@ for($i=0; $i < $N; $i++)
 
 	
 	
-	mysql_query("insert into files (floc,fdatein,fdesc,class_id,fname,teacher_id) value('$floc',NOW(),'$fdesc','$class_id','$fname','$session_id')")or die(mysql_error());
+	mysqli_query($GLOBALS["___mysqli_ston"], "insert into files (floc,fdatein,fdesc,class_id,fname,teacher_id) value('$floc',NOW(),'$fdesc','$class_id','$fname','$session_id')")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 	
 	
 	}
