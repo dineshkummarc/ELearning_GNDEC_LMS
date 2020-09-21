0226d321303924fb5172e6ec31be2fdd06fb92e4
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 14:20:21 +0530

    update

--- timer.ajax.php
+++ timer.ajax.php
@@ -2,14 +2,14 @@
 include('dbcon.php');
 include('session.php');
 
-$sql = mysql_query("SELECT * FROM student_class_quiz WHERE student_id = '$session_id'")or die(mysql_error());
-$row = mysql_fetch_array($sql);
+$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM student_class_quiz WHERE student_id = '$session_id'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+$row = mysqli_fetch_array($sql);
 $quiz_time = $row['student_quiz_time'];
 
-$sqlp = mysql_query("SELECT * FROM class_quiz")or die(mysql_error());
-$rowp = mysql_fetch_array($sqlp);
+$sqlp = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM class_quiz")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
+$rowp = mysqli_fetch_array($sqlp);
 if($quiz_time <= $rowp['quiz_time'] AND $quiz_time > 0){
-	 mysql_query("UPDATE student_class_quiz SET student_quiz_time = ".$row['student_quiz_time']." - 1 WHERE student_id = '$session_id'")or die(mysql_error()); 
+	 mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE student_class_quiz SET student_quiz_time = ".$row['student_quiz_time']." - 1 WHERE student_id = '$session_id'")or die(mysqli_error($GLOBALS["___mysqli_ston"])); 
 	/* $_SESSION['take_exam'] = 'continue'; */
 
 	$init = $quiz_time;
