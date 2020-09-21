d0c19091160bb94fecbdd21755381c61f084fc27
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 15:25:29 +0530

    update

--- delete_class_quiz.php
+++ delete_class_quiz.php
@@ -6,7 +6,7 @@ $id=$_POST['selector'];
 $N = count($id);
 for($i=0; $i < $N; $i++)
 {
-	$result = mysql_query("DELETE FROM class_quiz where class_quiz_id='$id[$i]'")or die(mysql_error());
+	$result = mysqli_query($GLOBALS["___mysqli_ston"], "DELETE FROM class_quiz where class_quiz_id='$id[$i]'")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 }
 ?>
 <script>
