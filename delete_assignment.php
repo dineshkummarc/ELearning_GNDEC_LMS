9c27476641b730bf38444dc1f17e5274fcb4d78f
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-21 15:33:46 +0530

    update

--- delete_assignment.php
+++ delete_assignment.php
@@ -2,7 +2,7 @@
 include('admin/dbcon.php');
 $id = $_POST['id'];
 $get_id = $_POST['get_id'];
-mysql_query("delete from assignment where assignment_id = '$id' ")or die(mysql_error());
+mysqli_query($GLOBALS["___mysqli_ston"], "delete from assignment where assignment_id = '$id' ")or die(mysqli_error($GLOBALS["___mysqli_ston"]));
 ?>
 <script>
 	window.location = 'assignment.php<?php echo '?id='.$get_id; ?>'
